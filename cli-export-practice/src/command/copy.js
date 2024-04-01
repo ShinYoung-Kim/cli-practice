f88a3a4f88de6302d7b622d0c2b7696a81a580e9
ShinYoung-Kim <lukey0515@naver.com>
2024-04-02 01:20:42 +0900

    feat: writefile 연습

---
+++ cli-export-practice/src/command/copy.js
@@ -0,0 +1,12 @@
+import { Command } from "commander";
+import fs from "fs";
+
+export const copyButton = () => {
+	fs.writeFile("copyButton.txt", "hi", () => {});
+};
+
+export const copy = new Command()
+	.name("copy")
+	.description("원하는 컴포넌트를 copy합니다.")
+	.action(async () => await copyButton())
+	.parse();
