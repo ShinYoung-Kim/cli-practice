c6f6cde02de8b9851503489623a66c8320665c72
ShinYoung-Kim <lukey0515@naver.com>
2024-04-02 15:34:40 +0900

    feat: 컴포넌트 write file 실습

--- cli-export-practice/src/command/copy.js
+++ cli-export-practice/src/command/copy.js
@@ -1,8 +1,31 @@
 import { Command } from "commander";
 import fs from "fs";
+import { Project, ScriptKind } from "ts-morph";
 
-export const copyButton = () => {
-	fs.writeFile("copyButton.txt", "hi", () => {});
+export const getButtonContent = async () => {
+	const files = [
+		{
+			name: "button.tsx",
+			content: "function Button() { \n return <button>버튼입니다</button>;\n}\n\nexport default Button;",
+		},
+	];
+
+	const project = new Project({});
+	const sourceFile = await project.createSourceFile(
+		"CopyButton.tsx",
+		files[0].content,
+		{
+			scriptKind: ScriptKind.TSX,
+		}
+	);
+
+	return await sourceFile.getFullText();
+};
+
+export const copyButton = async () => {
+	const content = await getButtonContent();
+	console.log(content);
+	fs.writeFile("copyButton.tsx", content, () => {});
 };
 
 export const copy = new Command()
---
+++ copyButton.tsx
@@ -0,0 +1,5 @@
+function Button() {
+	return <button>버튼입니다</button>;
+}
+
+export default Button;
