5b1c8adeb6b85ab4ab8a2a3d8c030193c56d0915
ShinYoung-Kim <lukey0515@naver.com>
2024-04-25 16:18:13 +0900

    chore: library build 코드

--- primitives/packages/lib-builder/lib/lib-builder.js
+++ primitives/packages/lib-builder/lib/lib-builder.js
@@ -1,1 +1,42 @@
-console.log("Hello Bob the builder");
+const rollup = require("rollup");
+const path = require("path");
+const resolve = require("@rollup/plugin-node-resolve").default;
+const babel = require("@rollup/plugin-babel").default;
+const postcss = require("rollup-plugin-postcss");
+
+const currentWorkingPath = process.cwd();
+const { src, name } = require(path.join(currentWorkingPath, "package.json"));
+const inputPath = path.join(currentWorkingPath, src);
+const fileName = name.replace("@primitives/", "");
+const inputOptions = {
+	input: inputPath,
+	external: ["react"],
+	plugins: [
+		resolve(),
+		postcss({
+			modules: true,
+		}),
+		babel({
+			presets: ["@babel/preset-env", "@babel/preset-react"],
+			babelHelpers: "bundled",
+			exclude: "node_modules/**",
+		}),
+	],
+};
+const outputOptions = [
+	{
+		file: `dist/${fileName}.cjs.js`,
+		format: "cjs",
+	},
+	{
+		file: `dist/${fileName}.esm.js`,
+		format: "esm",
+	},
+];
+async function build() {
+	const bundle = await rollup.rollup(inputOptions);
+	outputOptions.forEach(async (options) => {
+		await bundle.write(options);
+	});
+}
+build();
