ae1947fd924caf8f81d7c578cd795eb16bd02df8
ShinYoung-Kim <lukey0515@naver.com>
2024-04-27 02:17:18 +0900

    feat: 스토리북 파일 확장자변경

--- primitives/packages/lib-button/lib/lib-button.stories.js
+++
@@ -1,5 +0,0 @@
-import Button from "./lib-button";
-
-export const primary = () => <Button>이것이 내가 만든 버튼이다 이거야</Button>;
-
-export default { title: "Button" };
---
+++ primitives/packages/lib-button/lib/lib-button.stories.jsx
@@ -0,0 +1,5 @@
+import Button from "./lib-button";
+
+export default { title: "Button" };
+
+export const primary = () => <Button>이것이 내가 만든 버튼이다 이거야</Button>;
