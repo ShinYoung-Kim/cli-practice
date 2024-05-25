dafb8bfa707e911fe09320ebcad6acd94d088845
ShinYoung-Kim <lukey0515@naver.com>
2024-05-25 23:17:38 +0900

    chore: 채널톡 plugin 클론코딩

--- turborepo-starter/packages/icon-generator/src/ui/index.tsx
+++ turborepo-starter/packages/icon-generator/src/ui/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-});
+import { createRoot } from 'react-dom/client';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import ExtractSuccess from './components/ExtractSuccess';
+import Home from './components/Home';
+import IconExtract from './components/IconExtract';
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <MemoryRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="extract" element={<IconExtract />} />
+      <Route path="extract_success" element={<ExtractSuccess />} />
+    </Routes>
+  </MemoryRouter>,
+);
