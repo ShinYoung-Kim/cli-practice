03abed0d68ce919842ba4e1ee4892ba1f7bc00d5
ShinYoung-Kim <lukey0515@naver.com>
2024-06-13 14:18:59 +0900

    feat: icon sprite 파일 생성 스크립트 작성

---
+++ turborepo-starter/packages/icons/gulpfile.js
@@ -0,0 +1,23 @@
+import gulp from "gulp";
+import svgSprite from "gulp-svg-sprite";
+import path from "path";
+
+const iconSpriteFilePath = path.join(process.cwd(), "sprites");
+
+const createIconSpriteFile = () => {
+	return gulp
+		.src("svg/**/*.svg")
+		.pipe(
+			svgSprite({
+				mode: {
+					symbol: {
+						dest: iconSpriteFilePath,
+						sprite: "icons.svg",
+					},
+				},
+			})
+		)
+		.pipe(gulp.dest("sprites"));
+};
+
+export default createIconSpriteFile;
