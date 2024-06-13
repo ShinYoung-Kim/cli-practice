70352804d386df9efa37c9fbfbffe7772f7959e2
ShinYoung-Kim <lukey0515@naver.com>
2024-06-14 03:13:22 +0900

    feat: icon sprite file 생성 경로 변경

--- turborepo-starter/packages/icons/gulpfile.js
+++ turborepo-starter/packages/icons/gulpfile.js
@@ -2,7 +2,7 @@ import gulp from "gulp";
 import svgSprite from "gulp-svg-sprite";
 import path from "path";
 
-const iconSpriteFilePath = path.join(process.cwd(), "sprites");
+const iconSpriteFilePath = path.join(process.cwd());
 
 const createIconSpriteFile = () => {
 	return gulp
