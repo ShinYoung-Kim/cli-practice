5689c5222bbd1dab9adfd68dbeca70a68b2a3e12
ShinYoung-Kim <lukey0515@naver.com>
2024-06-20 22:39:06 +0900

    chore: 개별 아이콘 컴포넌트 path 추가

--- turborepo-starter/packages/icons/constants/path.js
+++ turborepo-starter/packages/icons/constants/path.js
@@ -12,6 +12,10 @@ export const FORMATTED_INDIVIDUAL_ICONS_PATH_JS = path.join(
 	process.cwd(),
 	"turborepo-starter/packages/icons/files/individual/formatted"
 );
+export const DEFAULT_INDIVIDUAL_ICON_COMPONENTS_PATH = path.join(
+	process.cwd(),
+	"turborepo-starter/packages/icons/component/individual"
+);
 
 export const DEFAULT_SVG_SPRITES_PATH = path.join(
 	process.cwd(),
