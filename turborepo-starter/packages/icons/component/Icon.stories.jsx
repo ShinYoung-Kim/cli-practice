7a7e7dd8c77f0a5fb70fe8a0b7e0a94dbdf5eef1
ShinYoung-Kim <lukey0515@naver.com>
2024-06-22 19:21:46 +0900

    feat: svgr 컴포넌트 비교 스토리에 추가

--- turborepo-starter/packages/icons/component/Icon.stories.jsx
+++ turborepo-starter/packages/icons/component/Icon.stories.jsx
@@ -2,6 +2,7 @@ import SpriteIcon from "./sprite/SpriteIcon";
 import { iconNames } from "../constants/iconNames.js";
 import { toPascalCase } from "../script/utils/transform.js";
 import * as IndividualIcon from "./individual/index";
+import * as SVGRIcon from "./svgr/index.jsx";
 
 const IconList = () => {
 	return (
@@ -9,11 +10,13 @@ const IconList = () => {
 			{iconNames.map((name) => {
 				console.log(name);
 				const IndividualComponent = IndividualIcon[toPascalCase(name)];
+				const SVGRComponent = SVGRIcon[toPascalCase(name)];
 				return (
 					<div>
 						<span>{name}</span>
 						<SpriteIcon key={name} name={name} />
 						<IndividualComponent />
+						<SVGRComponent width={24} height={24} />
 					</div>
 				);
 			})}
